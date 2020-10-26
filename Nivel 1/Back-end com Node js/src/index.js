6c4dd1eecd2253e32fb971ff41482209cbf39020
savio <savio27.05.2001@gmail.com>
2020-10-26 19:46:55 -0300

    metodos http

--- Nivel 1/Back-end com Node js/src/index.js
+++ Nivel 1/Back-end com Node js/src/index.js
@@ -1,10 +1,51 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req,res) => {
+/*
+*Métodos HTTP
+*
+*GET: Buscar/Pegar informções do backend
+*POST: Criar uma informação no backend
+*PUT/PATH: Alterar uma informção no backend
+*DELETE: Apagar/excluir uma informação do backend
+*/
+
+app.use(express.json())
+
+app.get('/projects', (req,res) => {
     return res.json({
-        message: "ola teste"
+        projeto1: 'martelo',
+        projeto2: 'capivara'
     })
+})
+
+app.post('/projects', (req,res) => {
+    return res.json({
+        projeto1: 'martelo',
+        projeto2: 'capivara',
+        projeto3: 'estante'
+    })
+})
+
+app.put('/projects/:id', (req,res) => {
+
+    return res.json({
+        projeto1: 'martelo',
+        projeto2: 'capivara',
+        projeto3: 'estante',
+        projeto4: 'projeto4'
+    })
+
+})
+
+app.delete('/projects/:id', (req,res) => {
+
+    return res.json({
+        projeto1: 'martelo',
+        projeto2: 'capivara',
+        projeto3: 'estante',
+    })
+
 })
 
 app.listen(2222, () => {

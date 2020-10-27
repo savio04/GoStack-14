80561225b71404d17d49c19d4b9fff8663484d10
savio <savio27.05.2001@gmail.com>
2020-10-27 12:36:55 -0300

    first challenge

---
+++ Desafios/Desafio-Conceitos-do-Node-Js/src/app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const cors = require("cors");
+
+const { v4, validate } = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories)
+});
+
+app.post("/repositories", (request, response) => {
+  const {title, url, techs} = request.body
+
+  const repositore = {id: v4(), likes: 0, techs, title, url}
+
+  repositories.push(repositore)
+
+  return response.status(201).json(repositore)
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const {id} = request.params
+  const {title, url, techs} = request.body
+
+  if(!validate(id)){
+    return response.status(400).json({
+      erro: "Ocorreu um erro"
+    })
+  }
+
+  const repositoreIndex = repositories.findIndex(repositore => repositore.id == id)
+
+  const repositore = {id,likes: repositories[repositoreIndex].likes,title, url, techs}
+
+  repositories.splice(repositoreIndex,1,repositore)
+
+  return response.json(repositore)
+
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params
+
+  if(!validate(id)){
+    return response.status(400).json({
+      erro: "Ocorreu um erro"
+    })
+  }
+
+  const repositoreIndex = repositories.findIndex(repositore => repositore.id ==id)
+
+  repositories.splice(repositoreIndex,1)
+
+  return response.status(204).send()
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const {id} = request.params
+
+  if(!validate(id)){
+    return response.status(400).json({
+      erro: "Ocorreu um erro"
+    })
+  }
+  const repositoreIndex = repositories.findIndex(repositore => repositore.id == id)
+
+  repositories[repositoreIndex].likes = repositories[repositoreIndex].likes+1
+
+  return response.json({likes: repositories[repositoreIndex].likes})
+});
+
+module.exports = app;

d62718d024515e507ebf7bc2fcc2f3f48caa22c4
savio <savio27.05.2001@gmail.com>
2020-10-28 09:55:41 -0300

    components

---
+++ Nivel 1/Front-end com ReactJS/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Ola teste</h2>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ Nivel 1/Front-end com ReactJS/frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <header>
+            <h2>Ola header</h2>
+        </header>
+    )
+}
+
+export default Header

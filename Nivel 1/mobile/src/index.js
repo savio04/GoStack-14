f19a4f8d3c1b1948800fdf236489389347d74dc0
savio <savio27.05.2001@gmail.com>
2020-10-29 14:04:39 -0300

    difference between react-native and ReactJs

--- Nivel 1/mobile/src/index.js
+++ Nivel 1/mobile/src/index.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet, StatusBar, Text } from 'react-native'
 
 function App(){
     return(
-        <View></View>
+        <>
+            <StatusBar barStyle = 'light-content' backgroundColor = '#8A05BE' />
+            <View style = {styles.constainer}>
+                <Text style = {styles.texto}>Hello GOstack</Text>
+            </View>
+        </>
     )
 }
+
+const styles = StyleSheet.create({
+    constainer: {
+        flex: 1,
+        backgroundColor: '#8A05BE',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    texto: {
+        color: '#FFF',
+        fontSize: 20
+    }
+})
 
 export default App

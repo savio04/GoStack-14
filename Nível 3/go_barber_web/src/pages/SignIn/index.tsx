f11552293e5f21e1050f0c8a0587b158c003a426
savio <savio27.05.2001@gmail.com>
2020-12-14 08:37:09 -0300

    input errors

--- Nível 3/go_barber_web/src/pages/SignIn/index.tsx
+++ Nível 3/go_barber_web/src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useCallback,useRef} from 'react'
 import { Content, Container, Background } from './styles'
 import Logo from '../../assets/logo.svg'
 import { FiLogIn,FiMail,FiLock } from 'react-icons/fi'
@@ -6,17 +6,34 @@ import Button from '../../components/Button'
 import Input from '../../components/Input'
 import { Link } from 'react-router-dom'
 import { Form } from '@unform/web'
+import { FormHandles } from '@unform/core'
+import * as Yup from 'yup'
 
 const SignIn: React.FC = () => {
-  function handleSubmit(data:object){
-    console.log(data)
-  }
+
+  const formRef = useRef<FormHandles>(null)
+  
+  const handleSubmit = useCallback(async (data:object) => {
+
+    try{
+      const schema = Yup.object().shape({
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail valido'),
+        password: Yup.string().min(8, 'No minimo 8 digitos')
+      })
+
+      await schema.validate(data,{
+        abortEarly: true
+      })
+    }catch(err){
+      console.log(err)
+    }
+  }, [])
   return (
     <Container>
       <Content>
         <img src = {Logo} alt = 'GoBarber' />
 
-        <Form onSubmit = {handleSubmit} >
+        <Form ref = {formRef} onSubmit = {handleSubmit} >
           <h1>Faça seu logon</h1>
 
           <Input placeholder = 'E-mail' name = 'email' icon = {FiMail} />
--- Nível 3/go_barber_web/src/pages/SignUp/index.tsx
+++ Nível 3/go_barber_web/src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useCallback,useRef} from 'react'
 import { Content, Container, Background } from './styles'
 import Logo from '../../assets/logo.svg'
 import { FiArrowLeft,FiMail,FiUser,FiLock } from 'react-icons/fi'
@@ -6,12 +6,33 @@ import Button from '../../components/Button'
 import Input from '../../components/Input'
 import { Link } from 'react-router-dom'
 import { Form } from '@unform/web'
+import { FormHandles } from '@unform/core'
+import getValidationErrors from '../../Utils/getValidationErros'
+import * as Yup from 'yup'
 
 const SignUp: React.FC = () => {
 
-  function handleSubmit(data:object){
-    console.log(data)
-  }
+  const formRef = useRef<FormHandles>(null)
+  console.log(formRef)
+
+  const handleSubmit = useCallback(async (data:object) => {
+    try{
+      formRef.current?.setErrors({})
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail valido'),
+        password: Yup.string().min(8, 'No minimo 8 digitos')
+      })
+
+      await schema.validate(data,{
+        abortEarly: false
+      })
+    }catch(err){
+      const errors = getValidationErrors(err)
+      formRef.current?.setErrors(errors)
+    }
+  }, [])
   return (
     <Container>
 
@@ -20,12 +41,12 @@ const SignUp: React.FC = () => {
       <Content>
         <img src = {Logo} alt = 'GoBarber' />
 
-        <Form onSubmit ={handleSubmit} >
+        <Form ref ={formRef} onSubmit ={handleSubmit} >
           <h1>Faça seu cadastro</h1>
 
-          <Input name = 'nome' placeholder = 'Nome' icon = {FiUser} />
+          <Input name = 'name' placeholder = 'Nome' icon = {FiUser} />
           <Input name = 'email' placeholder = 'E-mail' icon = {FiMail} />
-          <Input name = 'senha' type = 'password' placeholder = 'Senha' icon = {FiLock} />
+          <Input name = 'password' type = 'password' placeholder = 'Senha' icon = {FiLock} />
 
           <Button type = 'submit'>Enviar</Button>
         </Form>
